dfd548f9b4497ac67620e43e844fa689e46c4233
Paul DelRe <pdelre@gmail.com>
2014-03-21 16:58:25 +0000

    Restrict the Vote

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -3,10 +3,30 @@ App.Models.Question = Backbone.Model.extend({
 	defaults: {
 		votes: []
 	},
+	
+	constrain: function(value, min, max){
+		if(value < min) return min;
+		if(value > max) return max;
+		return value;
+	},
+	
+	getExistingVote: function(username){
+		return _(this.get('votes')).find(function(vote){
+			return vote.voter == username;
+		});
+	},
 
 	vote: function(username, voteType){
 		var voteValue = (voteType == 'up' ? 1 : -1);
-		this.get('votes').push({value: voteValue, voter: username});
+		var existingVote = this.getExistingVote(username);
+		
+		if(existingVote){
+			existingVote.value = this.constrain(existingVote.value, -1, 1);
+		}
+		else{		
+			this.get('votes').push({value: voteValue, voter: username});
+		}
+		
 		this.save();
 	},
 	
