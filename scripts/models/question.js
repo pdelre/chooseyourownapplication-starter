20ea3d821cd2ba54792a3dd4650fc97fed64b34b
Paul DelRe <pdelre@gmail.com>
2014-03-21 16:49:52 +0000

    Record the Vote

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -1,3 +1,19 @@
 App.Models.Question = Backbone.Model.extend({
+	
+	defaults: {
+		votes: []
+	},
 
+	vote: function(username, voteType){
+		var voteValue = (voteType == 'up' ? 1 : -1);
+		this.get('votes').push({value: voteValue, voter: username});
+		this.save();
+	},
+	
+	voteTally: function(){
+		return this.get('votes').reduce(function(tally, vote){
+			return tally + vote.value;
+		}, 0);
+	}
+	
 });
--- scripts/views/question.js
+++ scripts/views/question.js
@@ -3,9 +3,25 @@ App.Views.Question = Backbone.View.extend({
 	tagName: 'li',
 	className: 'question',
 	template: _.template($('#question-template').html()),
+	
+	events: {
+		'click .voteup': 'upVote',
+		'click .votedown': 'downVote'
+	},
+	
+	upVote: function(){ this.setVote('up'); },
+	downVote: function(){ this.setVote('down'); },
+	
+	setVote: function(voteType){
+		this.model.vote(App.currentUser.get('username'), voteType);
+		this.render();
+	},
 
 	render: function(){
-		this.$el.html(this.template(this.model.toJSON()));
+		var data = this.model.toJSON();
+		data.tally = this.model.voteTally();
+		
+		this.$el.html(this.template(data));
 		return this;
 	}
 
